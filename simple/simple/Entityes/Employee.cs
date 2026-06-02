65d1732373c3df7c1cb7e097cbd2c26c6dd184f5
Binoy kr <binoykrbar@gmail.com>
2026-06-02 23:40:28 +0530

    api development Initial commit

---
+++ simple/simple/Entityes/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace simple.Entityes
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? LastModificationDate { get; set; }
+        public DateTime? DOB { get; set; }
+        public string? Positios { get; set; }
+    }
+}
---
+++ simple/simple/Entityes/user.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace simple.Entityes
+{
+    public class user
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
